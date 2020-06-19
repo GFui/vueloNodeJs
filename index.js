58d178824333af90198cee5505c307607093cdd9
GFui <guille.valero15@gmail.com>
2020-06-19 14:32:02 +0200

    Añadida funcionalidad para buscar los vuelos de IDA en una bd MySQL

--- index.js
+++ index.js
@@ -12,21 +12,33 @@ var con = mysql.createConnection({
 
 app.use(express.static('public'));
 
+//Funcion para buscar los vuelos de IDA
+app.get('/vuelos/:origen/:destino/:salida', function (req, res) {
+	
+	var sql = "SELECT * FROM vuelos WHERE origen = ? AND destino = ? and salida like ?";
+	
+	var vuelo = {
+		origen: req.params.origen,
+		destino: req.params.destino,
+		salida: req.params.salida
+	}
+	
+	con.query(sql, [vuelo.origen, vuelo.destino, '%' + vuelo.salida +'%'], function (err, result) {
+		if (err) throw err;
+		
+		res.send(result);
+	});
+	
+});
+
 con.connect(function (err) {
 	if (err) throw err;
 
-	console.log("Connected!");
-
-	var sql = "SELECT * FROM aerolineas";
-
-	con.query(sql, function (err, result) {
-		if (err) throw err;
-		console.log(JSON.parse(JSON.stringify(result)));
-	})
+	console.log("Connected to MySQL!");
 
 });
 
 //Esto tiene que ir lo ultimo porque se queda bloqueando!
 app.listen(port, function () {
-	console.log(`Example app listening at http://localhost:${port}`);
+	console.log("VueloBonitoBarato listening at http://localhost:"+ port);
 });
--- public/js/controller.js
+++ public/js/controller.js
@@ -1,167 +1,194 @@
 angular.module('myApp', ['ngRoute'])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                controller: 'customersCtrl',
-                templateUrl: 'vistas/buscadorVuelos.html'
-            })
-            .when('/vuelo/:vueloSeleccionado', {
-                controller: 'vueloCtrl',
-                templateUrl: '/vistas/vueloView.html'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    })
-    .controller('customersCtrl', function ($scope, $http) {
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				controller: 'customersCtrl',
+				templateUrl: 'vistas/buscadorVuelos.html'
+			})
+			.when('/vuelo/:vueloSeleccionado', {
+				controller: 'vueloCtrl',
+				templateUrl: '/vistas/vueloView.html'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	})
+	.controller('customersCtrl', function ($scope, $http) {
 
-        $scope.Buscarvuelos = function () {
-            $scope.bandera = 1;
-            $http.get("js/vuelos.json").then(function (response) {
-                $scope.myData = response.data.vuelos;                
-            });
-        };
+		$scope.Buscarvuelos = function () {
 
-        $scope.formatearFecha = function (fecha) {
+			if ($scope.origen == "" || $scope.origen == null) {
+				$window.alert("No has seleccionado una ciudad de origen!");
+			} else if ($scope.salida == "" || $scope.salida == null) {
+				$window.alert("No has seleccionado fecha de salida!");
+			} else {
+				$scope.myData = [];
+				$scope.bandera = 1;
+				$http.get("vuelos/" + $scope.origen + '/' + $scope.destino + '/' + $scope.formatearFecha($scope.salida)).then(function (response) {
 
-            if (fecha != null) {
-                var d = new Date(fecha),
-                    month = '' + (d.getMonth() + 1),
-                    day = '' + d.getDate(),
-                    year = d.getFullYear();
+					$scope.myData = JSON.parse(JSON.stringify(response.data));
 
-                if (month.length < 2)
-                    month = '0' + month;
-                if (day.length < 2)
-                    day = '0' + day;
+				});
+			}
 
-                return [year, month, day].join('-');
-            }
+		};
 
-        };
+		$scope.formatearFecha = function (fecha) { //Este es para buscar en la db
+			
+		
+			if (fecha != null) {
+				var d = new Date(fecha),
+					month = '' + (d.getMonth() + 1),
+					day = '' + d.getDate(),
+					year = d.getFullYear();
 
-        $scope.cambioHora = function (horaJ) {
-            var fecha = new Date(horaJ);
-            //return tempsin[0];
-            return fecha + "";
-        };
+				if (month.length < 2)
+					month = '0' + month;
+				if (day.length < 2)
+					day = '0' + day;
+
+				return [year, month, day].join('-');
+			}
+
+		};
+
+		$scope.formatearFecha2 = function (fecha) { //Este para presentarla en tablas
+			console.log(fecha);
+			//2020-09-04T17:30:00.000Z
+			
+			var fechaYHora = fecha.split('T');
+			var soloFecha = fechaYHora[0];
+			var soloHora = fechaYHora[1].split('.')[0]; // para quitar el .00Z
+			
+			var horaTotal = soloFecha + ", a las " + soloHora;
+			return horaTotal;
+			
+
+		};
+
+		$scope.cambioHora = function (horaJ) {
+			var fecha = new Date(horaJ);
+			//return tempsin[0];
+			return fecha + "";
+		};
 
 
-    }).controller('vueloCtrl', function ($scope, $routeParams, $http) {
+	})
+	.controller('vueloCtrl', function ($scope, $routeParams, $http) {
 
-        $scope.billeteIda = JSON.parse($routeParams.vueloSeleccionado);
+		$scope.billeteIda = JSON.parse($routeParams.vueloSeleccionado);
 
-        $scope.numeroDeBilletes = 1;
+		$scope.numeroDeBilletes = 1;
 
-        $scope.addBillete = function (val) {
+		$scope.addBillete = function (val) {
 
-            $scope.numeroDeBilletes += val;
+			$scope.numeroDeBilletes += val;
 
-            if ($scope.numeroDeBilletes < 1) {
-                $scope.numeroDeBilletes = 1;
-            }
-        }
+			if ($scope.numeroDeBilletes < 1) {
+				$scope.numeroDeBilletes = 1;
+			}
+		}
 
-        $scope.opcionesVueloIda = [{
-            tipo: "Economy",
-            precio: $scope.billeteIda.economy
+		$scope.opcionesVueloIda = [{
+			tipo: "Economy",
+			precio: $scope.billeteIda.economy
             }, {
-            tipo: "Optima",
-            precio: $scope.billeteIda.optima
+			tipo: "Optima",
+			precio: $scope.billeteIda.optima
             }, {
-            tipo: "Bussiness ",
-            precio: $scope.billeteIda.bussiness
+			tipo: "Bussiness ",
+			precio: $scope.billeteIda.bussiness
             }];
 
-        $scope.opcionesIdaVuelta = [{
-            tipo: "Ida",
-            valor: 1
+		$scope.opcionesIdaVuelta = [{
+			tipo: "Ida",
+			valor: 1
                 }, {
-            tipo: "Ida y vuelta",
-            valor: 2
+			tipo: "Ida y vuelta",
+			valor: 2
                 }];
-        $scope.formatearFecha2 = function (fecha) {
+		$scope.formatearFecha = function (fecha) { //Este es para buscar en la db
 
-            if (fecha != null) {
-                var d = new Date(fecha),
-                    month = '' + (d.getMonth() + 1),
-                    day = '' + d.getDate(),
-                    year = d.getFullYear();
+			if (fecha != null) {
+				var d = new Date(fecha),
+					month = '' + (d.getMonth() + 1),
+					day = '' + d.getDate(),
+					year = d.getFullYear();
 
-                if (month.length < 2)
-                    month = '0' + month;
-                if (day.length < 2)
-                    day = '0' + day;
-                console.log([year, month, day].join('-'));
-                return [year, month, day].join('-');
-            }
+				if (month.length < 2)
+					month = '0' + month;
+				if (day.length < 2)
+					day = '0' + day;
 
-        };
+				return [year, month, day].join('-');
+			}
 
-        $scope.formatearFecha = function (fecha) {
+		};
 
-            if (fecha != null) {
-                var d = new Date(fecha),
-                    month = '' + (d.getMonth() + 1),
-                    day = '' + d.getDate(),
-                    year = d.getFullYear(),
-                    hour = d.getHours(),
-                    minutes = d.getMinutes();
+		$scope.formatearFecha2 = function (fecha) { //Este para presentarla en tablas
 
-                if (month.length < 2)
-                    month = '0' + month;
-                if (day.length < 2)
-                    day = '0' + day;
+			if (fecha != null) {
+				var d = new Date(fecha),
+					month = '' + (d.getMonth() + 1),
+					day = '' + d.getDate(),
+					year = d.getFullYear(),
+					hour = d.getHours(),
+					minutes = d.getMinutes();
 
-                var horaTotal = [day, month, year].join('-') + ", a las " + [hour, minutes].join(':')
-                return horaTotal;
-            }
+				if (month.length < 2)
+					month = '0' + month;
+				if (day.length < 2)
+					day = '0' + day;
 
-        };
+				var horaTotal = [day, month, year].join('-') + ", a las " + [hour, minutes].join(':')
+				return horaTotal;
+			}
 
-        $scope.getVuelta = function () {
-            $http.get("js/vuelos.json").then(function (response) {
-                $scope.vuelosVuelta = response.data.vuelos;
+		};
 
-            });
-        }();
+		$scope.getVuelta = function () {
+			$http.get("js/vuelos.json").then(function (response) {
+				$scope.vuelosVuelta = response.data.vuelos;
+
+			});
+		}();
 
 
-        $scope.escogerVuelta = function (x) {
+		$scope.escogerVuelta = function (x) {
 
-            $scope.billeteVuelta = x;
+			$scope.billeteVuelta = x;
 
-            $scope.opcionesVueloVuelta = [{
-                tipo: "Economy",
-                precio: $scope.billeteVuelta.economy
+			$scope.opcionesVueloVuelta = [{
+				tipo: "Economy",
+				precio: $scope.billeteVuelta.economy
             }, {
-                tipo: "Optima",
-                precio: $scope.billeteVuelta.optima
+				tipo: "Optima",
+				precio: $scope.billeteVuelta.optima
             }, {
-                tipo: "Bussiness",
-                precio: $scope.billeteVuelta.bussiness
+				tipo: "Bussiness",
+				precio: $scope.billeteVuelta.bussiness
             }];
 
 
-        }
+		}
 
-        $scope.getPrecioFinal = function () {
+		$scope.getPrecioFinal = function () {
 
-            if ($scope.claseSelecIda != null) {
-                if ($scope.billeteVuelta == null || $scope.claseSelecVuelta == null || $scope.idaVuelta.valor == 1) {
+			if ($scope.claseSelecIda != null) {
+				if ($scope.billeteVuelta == null || $scope.claseSelecVuelta == null || $scope.idaVuelta.valor == 1) {
 
-                    return ($scope.claseSelecIda.precio * $scope.numeroDeBilletes) + '€';
+					return ($scope.claseSelecIda.precio * $scope.numeroDeBilletes) + '€';
 
-                } else {
-                    return (($scope.claseSelecIda.precio + $scope.claseSelecVuelta.precio) * $scope.numeroDeBilletes) + '€';
-                }
+				} else {
+					return (($scope.claseSelecIda.precio + $scope.claseSelecVuelta.precio) * $scope.numeroDeBilletes) + '€';
+				}
 
-            }
-        }
-           $scope.finalizarCompra = function () {
-               
-                 $window.alert("Su compra se ha realizado con éxito, recibirá el producto en su dirección habitual");
-                
-            }
+			}
+		}
+		$scope.finalizarCompra = function () {
 
-    });
+			$window.alert("Su compra se ha realizado con éxito, recibirá el producto en su dirección habitual");
+
+		}
+
+	});
