3a99554346585e356ae03179e5b026cdcbc0341a
GFui <guille.valero15@gmail.com>
2020-06-19 13:49:05 +0200

    ?

--- index.js
+++ index.js
@@ -4,32 +4,29 @@ var app = express();
 var port = 3000;
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "guille",
-  password: "test",
-  database: "upct"
+	host: "localhost",
+	user: "guille",
+	password: "test",
+	database: "vuelosNode"
 });
-
 
 app.use(express.static('public'));
 
+con.connect(function (err) {
+	if (err) throw err;
 
-con.connect(function(err) {
-  if (err) throw err;
-    
-  console.log("Connected!");
+	console.log("Connected!");
 
-  /*var sql = "SELECT * FROM titulaciones";
-    
-  con.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log(result)
-  })*/
+	var sql = "SELECT * FROM aerolineas";
 
-    
- 
+	con.query(sql, function (err, result) {
+		if (err) throw err;
+		console.log(JSON.parse(JSON.stringify(result)));
+	})
+
 });
 
-app.listen(port, function(){
-	 console.log(`Example app listening at http://localhost:${port}`);
+//Esto tiene que ir lo ultimo porque se queda bloqueando!
+app.listen(port, function () {
+	console.log(`Example app listening at http://localhost:${port}`);
 });
